902388c978fdd013c7de37e4b76d8ecda466b92f
marcioc0sta <marciocst2@gmail.com>
2021-08-26 10:31:06 -0300

    feat: fetch events

--- src/redux/user-events.ts
+++ src/redux/user-events.ts
@@ -1,4 +1,4 @@
-import { createSlice } from '@reduxjs/toolkit'
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit'
 
 interface UserEvent {
   id: number
@@ -10,18 +10,46 @@ interface UserEvent {
 interface UserEventsSlice {
   byIds: Record<UserEvent['id'], UserEvent>
   allIds: UserEvent['id'][]
+  status: string | undefined
 }
 
 const initialState: UserEventsSlice = {
   byIds: {},
   allIds: [],
+  status: 'idle',
 }
+
+export const fetchUserEvents = createAsyncThunk<
+  UserEvent[],
+  void,
+  { rejectValue: string }
+>('users/fetchUserEvents', async (_, { rejectWithValue }) => {
+  try {
+    const response = await fetch('http://localhost:3001/events')
+    return await response.json()
+  } catch (e) {
+    return rejectWithValue('failed')
+  }
+})
 
 export const userEventsSlice = createSlice({
   name: 'userEvents',
   initialState,
   reducers: {
     initial: () => initialState,
+  },
+  extraReducers: builder => {
+    builder.addCase(fetchUserEvents.fulfilled, (state, action) => {
+      const events = action.payload
+      state.allIds = events.map(({ id }) => id)
+      state.byIds = events.reduce<UserEventsSlice['byIds']>((byIds, event) => {
+        byIds[event.id] = event
+        return byIds
+      }, {})
+    })
+    builder.addCase(fetchUserEvents.rejected, (state, action) => {
+      state.status = action.payload
+    })
   },
 })
 
